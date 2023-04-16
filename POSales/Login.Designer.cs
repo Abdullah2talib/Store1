d483b51d3852434c35a87f61943ee55dd9c9f717
saeah99shh <92373197+Sarah678935@users.noreply.github.com>
2023-04-16 21:39:55 +0300

    initial commitadd the connection string

--- POSales/Login.Designer.cs
+++ POSales/Login.Designer.cs
@@ -182,7 +182,6 @@ namespace POSales
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(66, 178);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(418, 367);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
